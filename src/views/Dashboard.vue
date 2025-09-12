a8dd3b94e9d4f4c584c63eaeaea496f7204c3425
mukesh-sharma <sharma.mukesh@thinksys.com>
2025-09-12 15:59:40 +0530

    fix the navbar plus icon

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,8 +5,8 @@ import SvgIcon from "@/components/SvgIcon.vue"; // Import your SVG component
 
 defineOptions({ name: "DashboardView" });
 
-const drawerOpen = ref(false)
-const isDarkTheme = ref(false)
+const drawerOpen = ref(false);
+const isDarkTheme = ref(false);
 
 const selectedFilters = ref({
   inquiries: false,
@@ -103,7 +103,7 @@ const items = ref([
 // Initialize theme
 onMounted(() => {
   document.documentElement.setAttribute("data-theme", "light");
-   document.documentElement.style.overflowY = 'auto';
+  document.documentElement.style.overflowY = "auto";
 });
 
 const toggleFilter = (filter) => {
@@ -144,32 +144,59 @@ const clearFilters = () => {
           />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="users" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="users"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="building" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="building"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="borderline"></div>
         <div class="sidebar-icon">
-          <SvgIcon name="user" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="user"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon" style="margin-left: 0.4rem">
-          <SvgIcon name="file" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="file"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon lock-icon">
-          <SvgIcon name="lock" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="lock"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="wrench" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="wrench"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="msg" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="msg"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="phone" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="phone"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="calender" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="calender"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon">
           <SvgIcon
@@ -178,10 +205,16 @@ const clearFilters = () => {
           />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="clipbord" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="clipbord"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="upgraph" :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'" />
+          <SvgIcon
+            name="upgraph"
+            :color="isDarkTheme ? '#374151' : 'rgb(205 204 204)'"
+          />
         </div>
         <div class="borderline"></div>
       </div>
@@ -194,7 +227,10 @@ const clearFilters = () => {
           />
         </div>
         <div class="sidebar-icon">
-          <SvgIcon name="info" :color="isDarkTheme ? '#374151' : 'rgb(205, 204, 204)'" />
+          <SvgIcon
+            name="info"
+            :color="isDarkTheme ? '#374151' : 'rgb(205, 204, 204)'"
+          />
         </div>
       </div>
     </aside>
@@ -208,11 +244,7 @@ const clearFilters = () => {
             <div class="search-icon">
               <SvgIcon name="search" size="sm" />
             </div>
-            <input
-              type="text"
-              placeholder="Search "
-              class="search-input"
-            />
+            <input type="text" placeholder="Search " class="search-input" />
           </div>
         </div>
         <div class="topbar-right">
@@ -220,15 +252,30 @@ const clearFilters = () => {
           <div class="topbar-actions">
             <div
               class="action-add-btn"
-              style="margin-bottom: .4rem;     margin-bottom: 0.1rem;"
+              style="margin-bottom: 0.4rem; margin-bottom: 0.1rem"
             >
-              <v-btn icon >
-                <SvgIcon name="plus"  />
+              <v-btn icon>
+                <!-- <SvgIcon name="plus"  /> -->
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="21"
+                  height="21"
+                  viewBox="0 0 35 35"
+                  fill="none"
+                >
+                  <!-- Circle -->
+                  <circle cx="17.5" cy="17.5" r="17.5" fill="#86CF37" />
+
+                  <!-- Plus icon (scaled & centered) -->
+                  <path
+                    d="M18.5 12.5C18.5 12.0858 18.1642 11.75 17.75 11.75C17.3358 11.75 17 12.0858 17 12.5V16H13.5C13.0858 16 12.75 16.3358 12.75 16.75C12.75 17.1642 13.0858 17.5 13.5 17.5H17V21C17 21.4142 17.3358 21.75 17.75 21.75C18.1642 21.75 18.5 21.4142 18.5 21V17.5H22C22.4142 17.5 22.75 17.1642 22.75 16.75C22.75 16.3358 22.4142 16 22 16H18.5V12.5Z"
+                    fill="black"
+                  />
+                </svg>
               </v-btn>
             </div>
 
             <!-- Notifications -->
-            
 
             <!-- Theme Toggle -->
             <!-- <div class="theme-toggle" @click="toggleTheme">
@@ -242,16 +289,18 @@ const clearFilters = () => {
 
           <!-- User Avatar Section -->
           <div class="avatar-section">
-            <div class="cursor-pointer" style="padding: 6px; padding: 2px 6px; border-radius: 8px;">
+            <div
+              class="cursor-pointer"
+              style="padding: 6px; padding: 2px 6px; border-radius: 8px"
+            >
               <SvgIcon name="bell" :color="isDarkTheme ? '#374151' : 'white'" />
             </div>
             <div class="user-info">
               Naomi R.
               <div class="avatar-ring">
-              <img src="../assets/icons/portrait06.png" alt="" />
+                <img src="../assets/icons/portrait06.png" alt="" />
+              </div>
             </div>
-            </div>
-            
           </div>
         </div>
       </div>
@@ -277,8 +326,11 @@ const clearFilters = () => {
                   <SvgIcon name="updownarrow" size="sm" />
                 </div>
               </div>
-              <div @click="drawerOpen = true" class="cursor-pointer drawer-open">
-                <SvgIcon name="drawer-open"/>
+              <div
+                @click="drawerOpen = true"
+                class="cursor-pointer drawer-open"
+              >
+                <SvgIcon name="drawer-open" />
               </div>
             </div>
           </div>
@@ -304,7 +356,7 @@ const clearFilters = () => {
               <!-- Hard Hat Card -->
               <div class="card">
                 <div class="icon-circle">
-                  <SvgIcon  style="width: 19px; height: 21px" />
+                  <SvgIcon style="width: 19px; height: 21px" />
                 </div>
                 <div class="card-info">
                   <div class="label">16</div>
@@ -336,7 +388,7 @@ const clearFilters = () => {
 
               <div class="verticalBorder"></div>
               <!-- Add New Card -->
-              <div class="add-card" >
+              <div class="add-card">
                 <div class="action-add-btn">
                   <v-btn icon size="small">
                     <SvgIcon name="plus" color="white" />
@@ -349,10 +401,7 @@ const clearFilters = () => {
           <!-- Filter Buttons and Search -->
           <div class="filters">
             <div class="filters-left">
-              <button
-                class="btn btn-active"
-                @click="toggleFilter('inquiries')"
-              >
+              <button class="btn btn-active" @click="toggleFilter('inquiries')">
                 Inquiries
               </button>
               <button
@@ -384,11 +433,8 @@ const clearFilters = () => {
                 Not Interested
               </button>
 
-              <button
-                class="btn"
-                style="padding: 0;"
-              >
-               <SvgIcon name="simpleplus" size="md"/>
+              <button class="btn" style="padding: 0">
+                <SvgIcon name="simpleplus" size="md" />
               </button>
             </div>
 
@@ -407,23 +453,14 @@ const clearFilters = () => {
                 <SvgIcon size="sm" />
                 Filter
               </button> -->
-              <button 
-                class="btn btn-plain"
-                style="padding: 6px; gap: 0px;"
-              >
-                <SvgIcon name="search" style="height: 15px; width: 15px;"  />
+              <button class="btn btn-plain" style="padding: 6px; gap: 0px">
+                <SvgIcon name="search" style="height: 15px; width: 15px" />
 
-                <SvgIcon name="v-filter" style="height: 15px; width: 15px;" />
+                <SvgIcon name="v-filter" style="height: 15px; width: 15px" />
               </button>
-              <button 
-                class="btn btn-plain"
-                @click="clearFilters"
-              >
-                <SvgIcon name="sort" style="width: 15px; height: 15px;"/>
+              <button class="btn btn-plain" @click="clearFilters">
+                <SvgIcon name="sort" style="width: 15px; height: 15px" />
               </button>
-
-              
-              
             </div>
           </div>
 
@@ -435,12 +472,55 @@ const clearFilters = () => {
                   <th>
                     <input type="checkbox" />
                   </th>
-                  <th><div class="table-head"><SvgIcon name="updownarrow" style="margin-bottom: -5px; margin-right: -4px;"/> Name</div></th>
-                  <th><div class="table-head"><SvgIcon name="updownarrow" style="margin-bottom: -5px; margin-right: -4px;" />Property</div></th>
-                  <th><div class="table-head"><SvgIcon name="updownarrow" style="margin-bottom: -5px; margin-right: -4px;" />Status</div></th>
-                  <th><div class="table-head"><SvgIcon name="updownarrow" style="margin-bottom: -5px; margin-right: -4px;" />Play / Trigger</div></th>
-                  <th><div class="table-head"><SvgIcon name="updownarrow" style="margin-bottom: -5px; margin-right: -4px;" />Date of Inquiry</div></th>
-                  <th class="text-center"><div class="table-head"><SvgIcon name="updownarrow" style="margin-bottom: -5px; margin-right: -4px;" />Actions</div></th>
+                  <th>
+                    <div class="table-head">
+                      <SvgIcon
+                        name="updownarrow"
+                        style="margin-bottom: -5px; margin-right: -4px"
+                      />
+                      Name
+                    </div>
+                  </th>
+                  <th>
+                    <div class="table-head">
+                      <SvgIcon
+                        name="updownarrow"
+                        style="margin-bottom: -5px; margin-right: -4px"
+                      />Property
+                    </div>
+                  </th>
+                  <th>
+                    <div class="table-head">
+                      <SvgIcon
+                        name="updownarrow"
+                        style="margin-bottom: -5px; margin-right: -4px"
+                      />Status
+                    </div>
+                  </th>
+                  <th>
+                    <div class="table-head">
+                      <SvgIcon
+                        name="updownarrow"
+                        style="margin-bottom: -5px; margin-right: -4px"
+                      />Play / Trigger
+                    </div>
+                  </th>
+                  <th>
+                    <div class="table-head">
+                      <SvgIcon
+                        name="updownarrow"
+                        style="margin-bottom: -5px; margin-right: -4px"
+                      />Date of Inquiry
+                    </div>
+                  </th>
+                  <th class="text-center">
+                    <div class="table-head">
+                      <SvgIcon
+                        name="updownarrow"
+                        style="margin-bottom: -5px; margin-right: -4px"
+                      />Actions
+                    </div>
+                  </th>
                 </tr>
               </thead>
               <tbody>
@@ -455,7 +535,7 @@ const clearFilters = () => {
                   <td>{{ item.date }}</td>
                   <td class="text-center">
                     <div class="cursor-pointer">
-                      <SvgIcon name="dots-vertical"  />
+                      <SvgIcon name="dots-vertical" />
                     </div>
                   </td>
                 </tr>
@@ -470,16 +550,22 @@ const clearFilters = () => {
                 <span>Rows per page:</span>
                 <span style="font-weight: 500">9</span>
                 <div class="cursor-pointer">
-                  <SvgIcon name="bottomarrow" style="width: 10px; height: 10px;" />
+                  <SvgIcon
+                    name="bottomarrow"
+                    style="width: 10px; height: 10px"
+                  />
                 </div>
               </div>
               <span style="font-weight: 500">1-4 of 36</span>
               <div class="pagination-control">
                 <div class="cursor-pointer">
-                  <SvgIcon name="leftarrow"  style="width: 10px; height: 10px;"/>
+                  <SvgIcon name="leftarrow" style="width: 10px; height: 10px" />
                 </div>
                 <div class="cursor-pointer">
-                  <SvgIcon name="rightarrow" style="width: 10px; height: 10px;" />
+                  <SvgIcon
+                    name="rightarrow"
+                    style="width: 10px; height: 10px"
+                  />
                 </div>
               </div>
             </div>
